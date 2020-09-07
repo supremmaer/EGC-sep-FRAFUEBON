953512ad8accf849e9f91d17bbe84e2bbb1066d6
supremmaer <xavy14op@gmail.com>
2020-09-07 17:58:37 +0200

    ejercicio2

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -13,24 +13,26 @@ MODULES = [
     'voting',
 ]
 
+BASEURL = 'http://localhost:8005'
+
 APIS = {
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
 }
 
-BASEURL = 'http://localhost:8000'
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'decide',
         'USER': 'decide',
         'PASSWORD': 'decide',
         'HOST': 'localhost',
