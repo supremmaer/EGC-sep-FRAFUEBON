0b77385fd0f299108655bf1e0fea787fdf1356bf
Andres <ajramirez@us.es>
2020-09-07 13:11:34 +0000

    primer commit

---
+++ docker/docker-settings1.py
@@ -0,0 +1,42 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+STATIC_ROOT = '/app/static/'
+MEDIA_ROOT = '/app/static/media/'
+ALLOWED_HOSTS = ['*']
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+BASEURL = 'http://10.5.0.1'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
---
+++ docker/docker-settings2.py
@@ -0,0 +1,42 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'decide'
+        'USER': 'decide',
+        'HOST': 'bd',
+        'PORT': 5432,
+    }
+}
+
+STATIC_ROOT = '/app/static/'
+MEDIA_ROOT = '/app/static/media/'
+ALLOWED_HOSTS = ['*']
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+BASEURL = 'http://localhost:8000'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
